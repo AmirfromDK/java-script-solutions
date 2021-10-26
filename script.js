2a1aba4ce49873cfa0e499c47189a6c5e12033b5
amirmvahed <amirmvahed@gmail.com>
2021-10-26 15:12:38 +0330

    recursiveSummation function

--- script.js
+++ script.js
@@ -11,3 +11,19 @@ const fibo = (input) => {
 }
 
 // console.log(fibo(4))
+
+
+const recursiveSummation = (input) => {
+    if (typeof input !== 'number') {
+        return 'please enter a number'
+    }
+
+    if (input <= 1) {
+        return input
+    }
+
+    return recursiveSummation(input - 1) + input
+
+}
+
+console.log(recursiveSummation(3))
